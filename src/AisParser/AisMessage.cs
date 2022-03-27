96c5cd933e4fed54ee63e6123e365539d4121616
Aleksander Wolak <a.wolak@am.szczecin.pl>
2022-03-27 21:46:26 +0200

    changes for entity framework

--- src/AisParser/AisMessage.cs
+++ src/AisParser/AisMessage.cs
@@ -1,7 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+
 namespace AisParser
 {
+    //for indexing
+    [Index(nameof(DateTimeUTC))]
+    [Index(nameof(Mmsi))]
     public abstract class AisMessage
     {
+        //required for entity framework index
+        public long Id { get; set; }
+        public DateTime DateTimeUTC { get; set; }
+        public string Source { get; set; }
+
         public AisMessageType MessageType { get; }
         public uint Repeat { get; set; }
         public uint Mmsi { get; set; }
